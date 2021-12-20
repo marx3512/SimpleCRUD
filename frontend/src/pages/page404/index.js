be3e4da1cbbcb44de8275c00868bc1f8befaac4f
marx3512 <marxborgesmachado@gmail.com>
2021-12-20 12:33:01 -0300

    Delete Front

--- frontend/src/pages/page404/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { Container } from '../../styles/GlobalStyles';
-
-const Page404 = function () {
-  return <Container>Essa pagina não existe</Container>;
-};
-
-export default Page404;
